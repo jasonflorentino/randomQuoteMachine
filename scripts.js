34d01b3eeba5476a5c5717aa93e88288e674d837
jasonflorentino <jasonmflorentino@gmail.com>
2021-01-18 10:31:16 -0500

    💃 Styled up + some animation!

--- scripts.js
+++ scripts.js
@@ -1,5 +1,79 @@
 'use strict';
 
+/*   COMPONENTS   */
+
+function Text(props) {
+  
+  return (
+    <div id="text">
+      <h2>{props.quote}</h2>
+    </div>
+  );
+}
+
+function Author(props) {
+  return (
+    <div id="author">
+      <h3>— {props.author}</h3>
+    </div>
+  );
+}
+
+function NewQuote(props) {
+  return (
+    <button id="new-quote" onClick={props.onClick}>
+      New Quote
+    </button>
+  );
+}
+
+function TweetQuote(props) {
+  return (
+    <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank">Tweet This</a>
+  );
+}
+
+/*   MAIN COMPONENT   */
+
+class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: "",
+    };
+  }
+  
+  getRandomQuote() {
+    const qLen = Object.keys(quotes).length
+    const randi = Math.floor((Math.random() * qLen))
+    const randQuote = quotes[Object.keys(quotes)[randi]]
+    
+    this.setState({
+      quote: randQuote.quote,
+      author: randQuote.author
+    });
+  }
+  
+  componentDidMount() {
+    console.log("mounted")
+    this.getRandomQuote()
+  }
+  
+  render() {
+    return (
+      <div id="quote-box" key={this.state.quote} className="fade-in">
+        <Text quote={this.state.quote} />
+        <Author author={this.state.author} />
+        <div id="links">
+          <TweetQuote />
+          <NewQuote onClick={() => this.getRandomQuote()} />
+        </div>
+      </div>
+    )
+  };
+}
+
 /*   DATA   */
 
 const quotes = {
@@ -91,79 +165,6 @@ const quotes = {
     quote: "Giving you the Ooh-ah-ah sensation!",
     author: "Monique Heart"
   },
-}
-
-/*   COMPONENTS   */
-
-function Text(props) {
-  return (
-    <div id="text">
-      <h2>{props.quote}</h2>
-    </div>
-  );
-}
-
-function Author(props) {
-  return (
-    <div id="author">
-      <h3>— {props.author}</h3>
-    </div>
-  );
-}
-
-function NewQuote(props) {
-  return (
-    <button id="new-quote" onClick={props.onClick}>
-      New Quote
-    </button>
-  );
-}
-
-function TweetQuote(props) {
-  return (
-    <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank">Tweet This</a>
-  );
-}
-
-/*   MAIN COMPONENT   */
-
-class QuoteBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: "",
-      author: "",
-    };
-  }
-  
-  getRandomQuote() {
-    const qLen = Object.keys(quotes).length
-    const randi = Math.floor((Math.random() * qLen))
-    const randQuote = quotes[Object.keys(quotes)[randi]]
-    
-    this.setState({
-      quote: randQuote.quote,
-      author: randQuote.author
-    });
-  }
-  
-  componentDidMount() {
-    console.log("mounted")
-    this.getRandomQuote()
-  }
-  
-  render() {
-    return (
-      <div id="quote-box">
-        <Text quote={this.state.quote} />
-        <Author author={this.state.author} />
-        <div id="links">
-          <TweetQuote />
-          <NewQuote onClick={() => this.getRandomQuote()} />
-        </div>
-      </div>
-    )
-  };
 }
 
 /*   RENDER TO DOM   */
